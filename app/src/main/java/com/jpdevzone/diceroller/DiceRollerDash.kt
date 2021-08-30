996454c0d2910a8a6a571bc270ffde1834c7fe60
pepper90 <panayotovj@gmail.com>
2021-08-30 22:42:03 +0300

    Initial commit

---
+++ app/src/main/java/com/jpdevzone/diceroller/DiceRollerDash.kt
@@ -0,0 +1,11 @@
+package com.jpdevzone.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiceRollerDash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dice_roller_dash)
+    }
+}
